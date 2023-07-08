3027f5c9ef42ae3bf6cd2ff3eddf1c7f4f8761df
riski maulana rahman <riskimaulanarahman@gmail.com>
2023-07-09 01:52:27 +0800

    akses jadwal hd

--- resources/views/jadwal/index.blade.php
+++ resources/views/jadwal/index.blade.php
@@ -14,6 +14,7 @@
 <div class="card card-primary card-outline">
     <div class="card-header">
         <h3 class="card-title">Periode Perubahan</h3>
+        <input type="hidden" name="leveluser" id="leveluser" value="{{Auth::user()->level}}">
     </div>
     <div class="card-body p-0 table-responsive">
         <table class="table table-bordered" style="font-size:12px;">
@@ -76,7 +77,7 @@
                 <td>{{ ($alternatifs[$key]['jenis_tindakan'] == 'inisiasi') ? 3 : 2 }}</td>
                 <td>{{ $alternatifs[$key]['jenis_tindakan'] }}</td>
                 <td>
-                    <select name="tindaklanjut" class="tindaklanjut" data-key="{{$key}}">
+                    <select name="tindaklanjut" class="tindaklanjut" data-key="{{$key}}" {{(Auth::user()->level == 'admin') ? '' : "disabled"}}>
                         <option value="1">- Pilih -</option>
                         <option value="2" {{ ($alternatifs[$key]['tindaklanjut'] == 2) ? 'selected' : '' }}>Dijadwalkan</option>
                         <option value="3" {{ ($alternatifs[$key]['tindaklanjut'] == 3) ? 'selected' : '' }}>Dirujuk alasan klinis</option>
@@ -84,24 +85,24 @@
                         <option value="5" {{ ($alternatifs[$key]['tindaklanjut'] == 5) ? 'selected' : '' }}>Tidak layak HD</option>
                     </select>
                 </td>
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="senin" {{($alternatifs[$key]["senin"][0]["shift_1"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="senin" {{($alternatifs[$key]["senin"][1]["shift_2"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="senin" {{($alternatifs[$key]["senin"][2]["shift_3"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="selasa" {{($alternatifs[$key]["selasa"][0]["shift_1"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="selasa" {{($alternatifs[$key]["selasa"][1]["shift_2"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="selasa" {{($alternatifs[$key]["selasa"][2]["shift_3"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="rabu" {{($alternatifs[$key]["rabu"][0]["shift_1"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="rabu" {{($alternatifs[$key]["rabu"][1]["shift_2"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="rabu" {{($alternatifs[$key]["rabu"][2]["shift_3"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="kamis" {{($alternatifs[$key]["kamis"][0]["shift_1"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="kamis" {{($alternatifs[$key]["kamis"][1]["shift_2"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="kamis" {{($alternatifs[$key]["kamis"][2]["shift_3"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="jumat" {{($alternatifs[$key]["jumat"][0]["shift_1"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="jumat" {{($alternatifs[$key]["jumat"][1]["shift_2"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="jumat" {{($alternatifs[$key]["jumat"][2]["shift_3"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="sabtu" {{($alternatifs[$key]["sabtu"][0]["shift_1"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="sabtu" {{($alternatifs[$key]["sabtu"][1]["shift_2"] == 1) ? "checked" : ''}}></td> 
-                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="sabtu" {{($alternatifs[$key]["sabtu"][2]["shift_3"] == 1) ? "checked" : ''}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="senin" {{($alternatifs[$key]["senin"][0]["shift_1"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="senin" {{($alternatifs[$key]["senin"][1]["shift_2"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="senin" {{($alternatifs[$key]["senin"][2]["shift_3"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="selasa" {{($alternatifs[$key]["selasa"][0]["shift_1"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="selasa" {{($alternatifs[$key]["selasa"][1]["shift_2"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="selasa" {{($alternatifs[$key]["selasa"][2]["shift_3"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="rabu" {{($alternatifs[$key]["rabu"][0]["shift_1"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="rabu" {{($alternatifs[$key]["rabu"][1]["shift_2"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="rabu" {{($alternatifs[$key]["rabu"][2]["shift_3"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="kamis" {{($alternatifs[$key]["kamis"][0]["shift_1"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="kamis" {{($alternatifs[$key]["kamis"][1]["shift_2"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="kamis" {{($alternatifs[$key]["kamis"][2]["shift_3"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="jumat" {{($alternatifs[$key]["jumat"][0]["shift_1"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="jumat" {{($alternatifs[$key]["jumat"][1]["shift_2"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="jumat" {{($alternatifs[$key]["jumat"][2]["shift_3"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="1" data-hari="sabtu" {{($alternatifs[$key]["sabtu"][0]["shift_1"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="2" data-hari="sabtu" {{($alternatifs[$key]["sabtu"][1]["shift_2"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
+                <td><input type="checkbox" onclick="tambahshift(this,'{{$key}}',this.getAttribute('data-shift'),this.getAttribute('data-hari'))" data-shift="3" data-hari="sabtu" {{($alternatifs[$key]["sabtu"][2]["shift_3"] == 1) ? "checked" : ''}} {{(Auth::user()->level == 'admin') ? '' : "disabled"}}></td> 
                 
                 <td>{{ $alternatifs[$key]['nama_K01'] }}</td>
                 <td>{{ $alternatifs[$key]['nama_K02'] }}</td>
@@ -126,88 +127,106 @@
 
 <script>
 $(document).ready(function() {
+    leveluser = $('#leveluser').val();
+
     $('.tindaklanjut').change(function() {
-        // Mendapatkan nilai dari elemen dropdown
-        var selectedValue = $(this).val();
-        var key = $(this).data('key');
 
-        // console.log(selectedValue)
-        // console.log(key)
+        if(leveluser == 'admin') {
 
-        // Membuat objek data untuk dikirim melalui AJAX
-        var data = {
-            tindaklanjut: selectedValue,
-            kode_alternatif: key
-        };
+            // Mendapatkan nilai dari elemen dropdown
+            var selectedValue = $(this).val();
+            var key = $(this).data('key');
 
-        console.log(data)
+            // console.log(selectedValue)
+            // console.log(key)
 
-        // Melakukan permintaan AJAX POST
-        $.ajax({
-          url: '/updatetindaklanjut',
-          type: 'POST',
-          data: data,
-          success: function(response) {
-            // Aksi yang dilakukan jika permintaan berhasil
-            alert('Data berhasil disimpan.');
-            location.reload()
-          },
-          error: function(xhr, status, error) {
-            // Aksi yang dilakukan jika permintaan gagal
-            console.log('Permintaan AJAX gagal');
-            console.log('Error:', error);
-          }
-        });
+            // Membuat objek data untuk dikirim melalui AJAX
+            var data = {
+                tindaklanjut: selectedValue,
+                kode_alternatif: key
+            };
+
+            console.log(data)
+
+            // Melakukan permintaan AJAX POST
+            $.ajax({
+            url: '/updatetindaklanjut',
+            type: 'POST',
+            data: data,
+            success: function(response) {
+                // Aksi yang dilakukan jika permintaan berhasil
+                alert('Data berhasil disimpan.');
+                location.reload()
+            },
+            error: function(xhr, status, error) {
+                // Aksi yang dilakukan jika permintaan gagal
+                console.log('Permintaan AJAX gagal');
+                console.log('Error:', error);
+            }
+            });
+
+        } else {
+            alert('Aksi Tidak di Izinkan')
+            location.reload();
+        }
     });
+
 });
 
+
 function tambahshift(check,key,shift,hari) {
-    if (check.checked) {
-        console.log('check')
-        console.log(key)
-        console.log(shift)
-        console.log(hari)
-        $.ajax({
-            url: '/updatejadwal',
-            type: 'POST',
-            data: {
-                kode:key,
-                hari:hari,
-                shift:shift,
-                value:1,
-                _token: $('input[name="_token"]').val(),
-            },
-            success: function(data) {
-                alert('Data berhasil disimpan.');
-                location.reload();
-            },
-            error: function(error) {
-                alert('Error.');
-            },
-        });
+    if(leveluser == 'admin') {
+        if (check.checked) {
+            console.log('check')
+            console.log(key)
+            console.log(shift)
+            console.log(hari)
+            $.ajax({
+                url: '/updatejadwal',
+                type: 'POST',
+                data: {
+                    kode:key,
+                    hari:hari,
+                    shift:shift,
+                    value:1,
+                    _token: $('input[name="_token"]').val(),
+                },
+                success: function(data) {
+                    alert('Data berhasil disimpan.');
+                    location.reload();
+                },
+                error: function(error) {
+                    alert('Error.');
+                },
+            });
+        } else {
+            console.log('uncheck')
+            console.log(key)
+            console.log(shift)
+            console.log(hari)
+            $.ajax({
+                url: '/updatejadwal',
+                type: 'POST',
+                data: {
+                    kode:key,
+                    hari:hari,
+                    shift:shift,
+                    value:0,
+                    _token: $('input[name="_token"]').val(),
+                },
+                success: function(data) {
+                    alert('Data berhasil disimpan.');
+                    location.reload();
+                },
+                error: function(error) {
+                    alert('Error.');
+                },
+            });
+        }
     } else {
-        console.log('uncheck')
-        console.log(key)
-        console.log(shift)
-        console.log(hari)
-        $.ajax({
-            url: '/updatejadwal',
-            type: 'POST',
-            data: {
-                kode:key,
-                hari:hari,
-                shift:shift,
-                value:0,
-                _token: $('input[name="_token"]').val(),
-            },
-            success: function(data) {
-                alert('Data berhasil disimpan.');
-                location.reload();
-            },
-            error: function(error) {
-                alert('Error.');
-            },
-        });
+        alert('Aksi Tidak di Izinkan')
+        location.reload();
     }
 }
+
 </script>
